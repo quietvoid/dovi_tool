a7fcea6940e84eb232d88cc875d316500b563a70
quietvoid <39477805+quietvoid@users.noreply.github.com>
2023-01-16 22:45:21 -0500

    Optimize `clear_start_code_emulation_prevention_3_byte`

--- dolby_vision/src/utils.rs
+++ dolby_vision/src/utils.rs
@@ -11,6 +11,8 @@ pub const ST2084_C1: f64 = 3424.0 / 4096.0;
 pub const ST2084_C2: f64 = (2413.0 / 4096.0) * 32.0;
 pub const ST2084_C3: f64 = (2392.0 / 4096.0) * 32.0;
 
+const THREE_BYTE_SLICE: &[u8] = &[0, 0, 3];
+
 /// Helper function to calculate PQ codes from nits (cd/m2) values
 #[inline(always)]
 pub fn nits_to_pq(nits: f64) -> f64 {
@@ -24,21 +26,19 @@ pub fn nits_to_pq(nits: f64) -> f64 {
 /// Unescapes a byte slice from annexb.
 /// Allocates a new Vec.
 pub fn clear_start_code_emulation_prevention_3_byte(data: &[u8]) -> Vec<u8> {
-    let mut unescaped_bytes: Vec<u8> = Vec::with_capacity(data.len());
+    let len = data.len();
+    let mut unescaped_bytes: Vec<u8> = Vec::with_capacity(len);
 
-    let unescaped_bytes_iter = data.iter().enumerate().filter_map(|(index, value)| {
-        if index > 2
-            && index < data.len() - 2
-            && data[index - 2] == 0
-            && data[index - 1] == 0
-            && data[index] == 3
-        {
-            None
-        } else {
-            Some(*value)
+    let mut i = 0;
+    while i < len {
+        let is_emulation_prevention_3_byte = i > 2 && matches!(&data[i - 2..=i], THREE_BYTE_SLICE);
+
+        if !is_emulation_prevention_3_byte {
+            unescaped_bytes.push(data[i]);
         }
-    });
-    unescaped_bytes.extend(unescaped_bytes_iter);
+
+        i += 1;
+    }
 
     unescaped_bytes
 }
