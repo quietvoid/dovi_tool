dc7ab6a41ae31707768eb4e2b4460267109dc4fa
quietvoid <39477805+quietvoid@users.noreply.github.com>
2023-05-28 10:47:12 -0400

    Properly support when missing .git dir

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ use vergen::EmitBuilder;
 
 fn main() {
     let mut emit_builder = EmitBuilder::builder();
+    emit_builder.fail_on_error();
     emit_builder.git_describe(true, true, Some("[0-9]*"));
 
     // Generate the instructions
