996a03b1c72e5e7127319405507ef97e7e6a3ea8
quietvoid <39477805+quietvoid@users.noreply.github.com>
2023-05-28 19:22:41 -0400

    Parse large RPU files incrementally
    Fixes #221

--- dolby_vision/src/rpu/utils.rs
+++ dolby_vision/src/rpu/utils.rs
@@ -11,76 +11,126 @@ use super::dovi_rpu::DoviRpu;
 pub fn parse_rpu_file<P: AsRef<Path>>(input: P) -> Result<Vec<DoviRpu>> {
     let rpu_file = File::open(input)?;
     let metadata = rpu_file.metadata()?;
-
-    // Should never be this large, avoid mistakes
-    if metadata.len() > 250_000_000 {
-        bail!("Input file probably too large");
-    }
+    let file_size_bytes = metadata.len() as usize;
 
     let mut reader = BufReader::new(rpu_file);
 
-    // Should be small enough to fit in the memory
-    let mut data = vec![0; metadata.len() as usize];
-    reader.read_exact(&mut data)?;
+    let chunk_size = 100_000;
+    let mut main_buf = vec![0; chunk_size];
+    let mut chunk = Vec::with_capacity(chunk_size);
+    let mut end = Vec::with_capacity(chunk_size);
 
-    let offsets: Vec<usize> = data
-        .windows(4)
-        .enumerate()
-        .filter_map(|(i, chunk)| {
-            if matches!(chunk, &[0, 0, 0, 1]) {
-                Some(i)
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    if offsets.is_empty() {
-        bail!("No NALU start codes found in the file. Maybe not a valid RPU?");
-    }
-
-    let count = offsets.len();
-    let last = *offsets.last().unwrap();
+    let mut offsets_count = 0;
+    // Estimate RPU count from file size
+    let mut rpus: Vec<DoviRpu> = Vec::with_capacity(chunk_size / 400);
     let mut warning_error = None;
 
-    let mut rpus: Vec<DoviRpu> = Vec::with_capacity(count);
-    let parsed_rpus_iter = offsets
-        .iter()
-        .enumerate()
-        .map(|(index, offset)| {
-            let size = if offset == &last {
-                data.len() - offset
-            } else {
-                offsets[index + 1] - offset
-            };
+    while let Ok(n) = reader.read(&mut main_buf) {
+        let read_bytes = n;
+        if read_bytes == 0 && end.is_empty() && chunk.is_empty() {
+            break;
+        }
 
-            let start = *offset;
-            let end = start + size;
+        if read_bytes < chunk_size {
+            chunk.extend_from_slice(&main_buf[..read_bytes]);
+        } else {
+            chunk.extend_from_slice(&main_buf);
+        }
 
-            DoviRpu::parse_unspec62_nalu(&data[start..end])
-        })
-        .enumerate()
-        .filter_map(|(i, res)| {
-            if let Err(e) = &res {
-                if warning_error.is_none() {
-                    warning_error = Some(format!("Found invalid RPU: Index {i}, error: {e}"))
+        let mut offsets: Vec<usize> = chunk
+            .windows(4)
+            .enumerate()
+            .filter_map(|(i, chunk)| {
+                if matches!(chunk, &[0, 0, 0, 1]) {
+                    Some(i)
+                } else {
+                    None
                 }
-            }
+            })
+            .collect();
 
-            res.ok()
-        });
-    rpus.extend(parsed_rpus_iter);
+        if offsets.is_empty() {
+            bail!("No NALU start codes found in chunk. Maybe not a valid RPU?");
+        }
 
-    if count > 0 && rpus.len() == count {
+        let last = if read_bytes < chunk_size {
+            *offsets.last().unwrap()
+        } else {
+            let last = offsets.pop().unwrap();
+
+            end.clear();
+            end.extend_from_slice(&chunk[last..]);
+
+            last
+        };
+
+        let count = offsets.len();
+        let parsed_rpus_iter = offsets
+            .iter()
+            .enumerate()
+            .map(|(index, offset)| {
+                let size = if offset == &last {
+                    chunk.len() - offset
+                } else {
+                    let size = if index == count - 1 {
+                        last - offset
+                    } else {
+                        offsets[index + 1] - offset
+                    };
+
+                    match &chunk[offset + size - 1..offset + size + 3] {
+                        [0, 0, 0, 1] => size - 1,
+                        _ => size,
+                    }
+                };
+
+                let start = *offset;
+                let end = start + size;
+
+                DoviRpu::parse_unspec62_nalu(&chunk[start..end])
+            })
+            .enumerate()
+            .filter_map(|(i, res)| {
+                if let Err(e) = &res {
+                    if warning_error.is_none() {
+                        warning_error = Some(format!("Found invalid RPU: Index {i}, error: {e}"))
+                    }
+                }
+
+                res.ok()
+            });
+        rpus.extend(parsed_rpus_iter);
+
+        if warning_error.is_some() {
+            offsets_count += count;
+            break;
+        } else if rpus.is_empty() {
+            bail!("No valid RPUs parsed for chunk, assuming invalid RPU file.");
+        }
+
+        if offsets_count == 0 && file_size_bytes > chunk_size {
+            rpus.reserve((metadata.len() as usize - chunk_size) / 400);
+        }
+        offsets_count += count;
+
+        chunk.clear();
+
+        if !end.is_empty() {
+            chunk.extend_from_slice(&end);
+            end.clear()
+        }
+    }
+
+    if offsets_count > 0 && rpus.len() == offsets_count {
         Ok(rpus)
-    } else if count == 0 {
+    } else if offsets_count == 0 {
         bail!("No RPU found");
     } else if let Some(error) = warning_error {
         bail!("{}", error);
     } else {
         bail!(
             "Number of valid RPUs different from total: expected {} got {}",
-            count,
+            offsets_count,
             rpus.len()
         );
     }
